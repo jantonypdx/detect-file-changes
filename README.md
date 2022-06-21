8b913208e66dc7a3ed330e4ac1f6ba1b7f6e588a
Antony, John <jantony@columbia.com>
2022-06-21 14:23:34 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# detect-file-changes
+
+This is a repository demonstrating how file changes can be 
+automatically detected & tracked. The repository can then easily 
+show the previous file contents as well as the new updated 
+data. Additionally, git notifications can be used to alert users
+whenever the file contents change.
+
+## Installation
+
+- git clone the repository
+- run `npm installl`
+
+## Usage
+Run the following command to ESLint test all javascript files in the repository:
+```
+npm run update-files
+```
+
+## License
+
+[MIT](./LICENSE.md)
---
+++ tests/getRobotsFile.mjs
@@ -0,0 +1,51 @@
+/* eslint-disable no-console */
+// This is a script to download robots file
+//
+// Usage:
+// node test/getRobotsFile.js
+//
+
+// eslint-disable-next-line import/no-unresolved
+import got from 'got';
+import fs from 'fs';
+import path from 'path';
+import simpleGit from 'simple-git';
+
+const git = simpleGit();
+
+// main function
+(async () => {
+  const site = 'https://www.google.com';
+  const fileName = 'robots.txt';
+  const fileUrl = `${site}/${fileName}`;
+
+  const filePath = './files';
+  const fileAbsolutePath = path.resolve(`${filePath}/${fileName}`);
+  const options = {};
+
+  console.log(`Getting robots file:`);
+
+  try {
+    // retrieve the page contents & write to file
+    const { body: fileData } = await got(fileUrl, options);
+    fs.writeFileSync(fileAbsolutePath, fileData);
+    console.log(`- ${fileUrl} -> ${filePath}/${fileName}`);
+
+    // look for git changes related to the file
+    const status = await git.status();
+    const modifiedFiles = status.modified.filter((elem) =>
+      elem.includes(fileName)
+    ).length;
+
+    // if file changes were detected, then add, commit, and push
+    // the changes to the remote repository
+    if (modifiedFiles > 0) {
+      const message = `\n${modifiedFiles} modified '${fileName}' file`;
+      console.log(message);
+    } else {
+      console.log(`\nNo '${fileName}' changes found.`);
+    }
+  } catch (error) {
+    console.log('Error:', error);
+  }
+})();
